00a4a5b7397838a14e0afce3fdb6746ab014d0c0
TNBC-AAP <kirti.jain@csir.res.in>
2025-07-22 15:17:34 +0530

    Add files via upload

---
+++ Panel-05/FS6/feature_selection_stepwise.R
@@ -0,0 +1,24 @@
+# script - Feature set - f (6 feature model) ---- how feature selection take place! 
+
+library(tidyverse)  
+library(caret)      
+library(leaps)      
+library(MASS)
+
+#reading input file
+final <- as.data.frame(read_csv("stepwise_regression_14_features_training.csv"))
+final <- as.data.frame(final)
+genes <- colnames(final)[11:dim(final)[2]]
+cox_model <- coxph(as.formula(paste("Surv(RFS_time_Months, RFS_Status) ~", paste(genes, collapse = " + "))), data = final)
+
+step.model <- stepAIC(cox_model, direction = "both", trace = TRUE)
+summary(step.model)
+# Extract selected genes
+selected_genes <- names(coef(step.model))
+coef_25 <- as.data.frame(coef(step.model))
+coef_25$gene <- rownames(coef_25)
+print(selected_genes)
+length(selected_genes)
+
+backward_gene_list = selected_genes
+length(backward_gene_list) #25
